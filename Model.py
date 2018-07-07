fd16b7172a2bea1b3d305711822c4f06a98c0533
CheshtaK <32400008+CheshtaK@users.noreply.github.com>
2018-07-07 20:05:14 +0530

    creating data for testing

--- Model.py
+++ Model.py
@@ -4,7 +4,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
 
+import random
+
 style.use('ggplot')
+
+def create_dataset(howmuch, variance, step = 2, correlation = False):
+    val = 1
+    ys = []
+    for i in range(howmuch):
+        y = val + random.randrange(-variance, variance)
+        ys.append(y)
+        if correlation and correlation == 'pos':
+            val += step
+        elif correlation and correlation == 'neg':
+            val -= step
+
+    xs = [i for i in range(len(ys))]
+    
+    return np.array(xs, dtype = np.float64), np.array(ys, dtype = np.float64)
 
 def best_fit_slope_and_intercept(xs, ys):
 
@@ -23,29 +40,31 @@ def coefficient_of_determination(ys_orig, ys_line):
     return 1 - (squared_error_regr / squared_error_mean)
 
 def main():
-    xs = [1,2,3,4,5]
-    ys = [5,4,6,5,6]
+##    xs = [1,2,3,4,5]
+##    ys = [5,4,6,5,6]
+##
+##    xs = np.array([1,2,3,4,5], dtype = np.float64)
+##    ys = np.array([5,4,6,5,6], dtype = np.float64)
 
-    xs = np.array([1,2,3,4,5], dtype = np.float64)
-    ys = np.array([5,4,6,5,6], dtype = np.float64)
-
+    xs, ys = create_dataset(40, 10, 2, correlation = 'pos')
+    
     m,b = best_fit_slope_and_intercept(xs, ys)
     print(m, b)
 
     regression_line = [(m*x) + b for x in xs]
 
-    predict_x = 7
-    predict_y = (m*predict_x) + b
-    print(predict_y)
+##    predict_x = 7
+##    predict_y = (m*predict_x) + b
+##    print(predict_y)
 
     r_squared = coefficient_of_determination(ys, regression_line)
     print(r_squared)
 
-##    plt.scatter(xs, ys, color = '#003F72', label = 'data')
-##    plt.plot(xs, regression_line, label = 'regression line')
+    plt.scatter(xs, ys, color = '#003F72', label = 'data')
+    plt.plot(xs, regression_line, label = 'regression line')
 ##    plt.scatter(predict_x, predict_y, color = '#2eb82e', label = 'prediction')
-##    plt.legend(loc=4)
-##    plt.show()
+    plt.legend(loc=4)
+    plt.show()
 
 if __name__ == '__main__':
     main()
